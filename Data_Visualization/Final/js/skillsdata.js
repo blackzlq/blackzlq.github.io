04bb0ef1f414215d20425b3799ff8796193856fc
blackzlq <zhang.liq@husky.neu.edu>
2017-04-05 00:06:46 -0400

    Add files via upload

---
+++ Data_Visualization/Final/js/skillsdata.js
@@ -0,0 +1,128 @@
+var skillsdata;
+ 
+skillsdata =
+{
+        "Skills":
+        {
+                "0Marr":
+                {
+                        "Gender":
+                        {
+                                "Men": [100, 56, 23, 16],
+                                "Women": [99, 39, 16, 11]
+                            },
+                        "Race":
+                        {
+                                "White": [99, 45, 15, 10],
+                                "Black": [100, 65, 40, 33],
+                                "Hispancis": [100, 45, 21, 16]
+                            },
+                        "Education":
+                        {
+                                "BelowHighSchool": [99, 42, 24, 19],
+                                "HighSchool": [99, 40, 18, 14],
+                                "College": [100, 44, 19, 13],
+                                "Bachelor&Higher": [100, 63, 19, 12]
+                            }
+                     },
+                
+                 "1stMarr":
+                {
+                        "Gender":
+                        {
+                                "Men": [0, 38, 55, 50],
+                                "Women": [0, 50, 54, 47]
+                                
+                            },
+                        "Race":
+                        {
+                                "White": [0, 30, 39, 34],
+                                "Black": [0, 65, 40, 33],
+                                "Hispancis": [0, 48, 55, 46]
+                            },
+                        "Education":
+                        {
+                                "BelowHighSchool": [0, 45, 44, 34],
+                                "HighSchool": [0, 49, 52, 45],
+                                "College": [0, 47, 51, 46],
+                                "Bachelor&Higher": [0, 35, 67, 63]
+                            }
+                     },
+                
+                 "1stEnd No2nd":
+                {
+                        "Gender":
+                        {
+                                "Men": [0, 4, 10, 12],
+                                "Women": [0, 7, 12, 14]
+                                
+                            },
+                        "Race":
+                        {
+                                "White": [0, 5, 11, 12],
+                                "Black": [0, 4, 13, 17],
+                                "Hispancis": [0, 6, 12, 16]
+                            },
+                        "Education":
+                        {
+                                "BelowHighSchool": [0, 8, 15, 19],
+                                "HighSchool": [0, 6, 12, 14],
+                                "College": [0, 6, 13, 15],
+                                "Bachelor&Higher": [0, 2, 6, 9]
+                            }
+                     },
+                
+                 "2ndMarr":
+                {
+                        "Gender":
+                        {
+                                "Men": [0, 2, 10, 15],
+                                "Women": [0, 4, 13, 17]
+                                
+                            },
+                        "Race":
+                        {
+                                "White": [0, 3, 12, 17],
+                                "Black": [0, 1, 6, 11],
+                                "Hispancis": [0, 2, 11, 17]
+                            },
+                        "Education":
+                        {
+                                "BelowHighSchool": [0, 3, 11, 16],
+                                "HighSchool": [0, 3, 13, 18],
+                                "College": [0, 3, 12, 16],
+                                "Bachelor&Higher": [0, 1, 7, 12]
+                            }
+                    },
+                "2ndEnd No3rd":
+                {
+                        "Gender":
+                        {
+                                "Men": [0, 0, 2, 4],
+                                "Women": [0, 1, 3, 5]
+                                
+                            },
+                        "Race":
+                        {
+                                "White": [0, 0, 2, 5],
+                                "Black": [0, 0, 1, 3],
+                                "Hispancis": [0, 0, 1, 4]
+                            },
+                        "Education":
+                        {
+                                "BelowHighSchool": [0, 1, 4, 5],
+                                "HighSchool": [0, 0, 2, 5],
+                                "College": [0, 0, 2, 5],
+                                "Bachelor&Higher": [0, 0, 1, 2]
+                        }
+                    }
+        
+                
+                
+            
+        
+                
+            
+            }
+};
+

58745fcfa47e9749172442b48bd247f44ee72f08
blackzlq <zhang.liq@husky.neu.edu>
2019-11-14 17:20:20 -0800

    Add files via upload

---
+++ hanqi/assets/js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2019, 4, 13),
+		newYear = true;
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			message += "hanqi 心动后已经 ";
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) 
+			message += " 没有take action了";
+			message += " <br />";
+
+			note.html(message);
+		}
+	});
+	
+});
